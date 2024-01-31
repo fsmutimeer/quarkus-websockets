3a8dd625279c5e4418cfa40999ce1fb7997f9349
Feroz Shah <feroz.shah@it22.nl>
2024-01-31 18:12:09 +0500

    basic chat app set

---
+++ src/main/java/org/acme/ChatSocket.java
@@ -0,0 +1,58 @@
+package org.acme;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.jboss.logging.Logger;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.websocket.OnClose;
+import jakarta.websocket.OnError;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.Session;
+import jakarta.websocket.server.PathParam;
+import jakarta.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/chat/{username}")
+@ApplicationScoped
+public class ChatSocket {
+    private static final Logger LOG = Logger.getLogger(ChatSocket.class);
+
+    Map<String, Session> sessions = new ConcurrentHashMap<>();
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("username") String username) {
+        broadcast("User " + username + " joined");
+        sessions.put(username, session);
+        LOG.info(username + " connected");
+    }
+
+    @OnClose
+    public void onClose(Session session, @PathParam("username") String username) {
+        sessions.remove(username, session);
+        broadcast("User " + username + " left");
+    }
+
+    @OnError
+    public void onError(Session session, @PathParam("username") String username, Throwable throwable) {
+        sessions.remove(username);
+        LOG.error("onError", throwable);
+        broadcast("User " + username + " left on error: " + throwable);
+    }
+
+    @OnMessage
+    public void onMessage(String message, @PathParam("username") String username) {
+        broadcast(username + ": " + message);
+    }
+
+    private void broadcast(String message) {
+        sessions.values().forEach(s -> {
+            s.getAsyncRemote().sendObject(message, result -> {
+                if (result.getException() != null) {
+                    System.out.println("Unable to send message: " + result.getException());
+                }
+            });
+        });
+    }
+}
